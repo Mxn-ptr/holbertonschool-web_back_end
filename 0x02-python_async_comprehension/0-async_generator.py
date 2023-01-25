c139c86567cbeb47216024cf30f1d605eec3fe0e
Mxn-ptr <maxence0@hotmail.fr>
2023-01-25 12:23:59 +0100

    Fix task à

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,8 +5,8 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """ Loop 10 times and yield a random number """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
