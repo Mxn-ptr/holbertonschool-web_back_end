7a25b17699b69f9a39a0b16a27a7d2d87cc8b69d
Mxn-ptr <maxence0@hotmail.fr>
2023-01-27 00:08:40 +0100

    Fix task 0

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,7 +9,7 @@ class BasicCache(BaseCaching):
         """ Assign the item value for the key to the dictionary """
         if key is not None or item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Return the value of key in the dictionary """
         if key in self.cache_data:
