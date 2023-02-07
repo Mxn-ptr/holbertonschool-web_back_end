c78de6eddaef1883249d7fd48f9e1d19425527ef
Mxn-ptr <maxence0@hotmail.fr>
2023-02-07 10:03:47 +0100

    Fix doc

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -43,6 +43,7 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request():
+    """ Execute before request """
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if auth is None:
