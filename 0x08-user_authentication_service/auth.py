002516458c51b1bdcaf9b1e6bfea20fc8efcfdb9
Mxn-ptr <maxence0@hotmail.fr>
2023-02-15 13:50:04 +0100

    Fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -76,7 +76,7 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except NoResultFound:
+        except Exception:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
