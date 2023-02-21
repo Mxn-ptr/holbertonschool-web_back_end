b4bd3fdffa5818bd0fa602beccb4ed7dd114fc29
Mxn-ptr <maxence0@hotmail.fr>
2023-02-21 13:11:42 +0100

    Add task 5

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Client module test"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -18,3 +18,12 @@ class TestGithubOrgClient(unittest.TestCase):
         test = GithubOrgClient(org)
         self.assertEqual(test.org, patched.return_value)
         patched.assert_called_once()
+
+    def test_public_repos_url(self):
+        """ Test """
+        expected = "https://api.github.com/orgs/google/repos"
+        payload = {"repos_url": expected}
+        patched = "client.GithubOrgClient.org"
+        with patch(patched, PropertyMock(return_value=payload)):
+            new = GithubOrgClient("google")
+            self.assertEqual(new._public_repos_url, expected)
