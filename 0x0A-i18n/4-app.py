3102f91301063b65611fd9afc756304e6d16560b
Mxn-ptr <maxence0@hotmail.fr>
2023-02-23 13:25:58 +0100

    Add task 4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" App """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config Class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ return the accept languages """
+    language = request.args.get('locale')
+    if language and language in app.config['LANGUAGES']:
+        return language
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ Return a template """
+    return render_template('4-index.html')
