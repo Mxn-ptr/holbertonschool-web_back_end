d995393d1fa72008f33967ad59f9cc34ff35dceb
Mxn-ptr <maxence0@hotmail.fr>
2023-03-07 13:36:54 +0100

    Add advanced task

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -3,13 +3,16 @@
 import requests
 import redis
 
+red = redis.Redis()
+
 
 def get_page(url: str) -> str:
     """ Track how many times a particular URL
      was accessed in the key "count:{url}" """
     count_key = f"count:{url}"
-    redis.incr(count_key)
-    redis.expire(count_key, 10)
+    count = red.incr(count_key)
+    if count == 1:
+        red.expire(count_key, 10)
 
     res = requests.get(url)
     return res.text
