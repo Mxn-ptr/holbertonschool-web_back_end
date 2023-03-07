c369984a27b113db4db6d0d96edc8c89250a02ea
Mxn-ptr <maxence0@hotmail.fr>
2023-03-07 13:41:01 +0100

    Add advanced task

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -2,17 +2,31 @@
 """ Web cache and tracker """
 import requests
 import redis
+from typing import Callable
+from functools import wraps
 
 red = redis.Redis()
 
 
+def count(method: Callable) -> Callable:
+    """ count method """
+
+    @wraps(method)
+    def wrapper(url):
+        """ wrapper method """
+        count_key = f"count:{url}"
+        count = red.incr(count_key)
+        if count == 1:
+            red.expire(count_key, 10)
+        res = method(url)
+        return res
+
+    return wrapper
+
+
+@count
 def get_page(url: str) -> str:
     """ Track how many times a particular URL
      was accessed in the key "count:{url}" """
-    count_key = f"count:{url}"
-    count = red.incr(count_key)
-    if count == 1:
-        red.expire(count_key, 10)
-
     res = requests.get(url)
     return res.text
