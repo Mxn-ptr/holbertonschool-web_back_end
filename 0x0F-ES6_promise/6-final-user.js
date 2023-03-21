66db9e98b3347f1125a26f9dbac546d67ece3236
Mxn-ptr <maxence0@hotmail.fr>
2023-03-21 11:38:36 +0100

    Fix task 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,5 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => res);
+    .then((res) => {
+      for (const e of res) {
+        if (e.status === 'rejected') {
+          e.value = e.reason;
+          delete e.reason;
+        }
+      }
+      return res;
+    });
 }
