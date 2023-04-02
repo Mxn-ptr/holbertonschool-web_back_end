15683ad53eb0ea47f345dc61c33e7e6855de2c16
Mxn-ptr <maxence0@hotmail.fr>
2023-04-02 19:13:19 +0200

    Fix error

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,26 +4,29 @@ module.exports = class StudentsController {
   static getAllStudents(request, response, database) {
     readDatabase(database)
       .then((data) => {
-        let line = 'This is the list of our students';
+        const line = [];
+        line.push('This is the list of our students');
         for (const field in data) {
           if (Object.hasOwn(data, field)) {
             const e = data[field];
-            line += `\nNumber of students in ${field}: ${e.count}. ${e.students}`;
+            line.push(`Number of students in ${field}: ${e.count}. ${e.students}`);
           }
         }
-        response.status(200).send(line);
+        response.status(200).send(line.join('\n'));
       })
       .catch((err) => response.status(500).send(err.message));
   }
 
   static getAllStudentsByMajor(request, response, database) {
-    if (request.params.major !== 'CS' && request.params.major !== 'SWE') {
+    const major = request.params;
+    if (major !== 'CS' && major !== 'SWE') {
       response.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      readDatabase(database)
+        .then((data) => {
+          response.status(200).send(data[request.params.major].students);
+        })
+        .catch((err) => response.status(500).send(err.message));
     }
-    readDatabase(database)
-      .then((data) => {
-        response.status(200).send(data[request.params.major].students);
-      })
-      .catch((err) => response.status(500).send(err.message));
   }
 };
