49b8851a891f6482a713d6d34580beaa5358a5ee
Mxn-ptr <maxence0@hotmail.fr>
2023-04-02 20:10:38 +0200

    Fix task

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,20 +2,19 @@ import readDatabase from '../utils';
 
 export default class StudentsController {
   static getAllStudents(request, response, database) {
-    response.write('This is the list of our students\n');
     readDatabase(database)
       .then((data) => {
         const line = [];
+        line.push('This is the list of our students');
         for (const field in data) {
           if (Object.hasOwn(data, field)) {
             const e = data[field];
             line.push(`Number of students in ${field}: ${e.count}. ${e.students}`);
           }
         }
-        response.status(200).end(line.join('\n'));
-        response.send();
+        response.status(200).send(line.join('\n'));
       })
-      .catch((err) => response.status(500).end(err.message));
+      .catch((err) => response.status(500).send(err.message));
   }
 
   static getAllStudentsByMajor(request, response, database) {
