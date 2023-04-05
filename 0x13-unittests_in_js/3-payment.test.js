d7fcfa382dec360752465530de4cb23eb631b80a
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 14:43:12 +0200

    FIx task 3

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,15 +1,15 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
+const utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('Result of payment is the same as the function', () => {
-    const functionSpy = sinon.spy(Utils, 'calculateNumber');
+    const functionSpy = sinon.spy(utils, 'calculateNumber');
     const apiResult = sendPaymentRequestToApi(100, 20);
     
     expect(functionSpy.calledOnceWith('SUM', 100, 20)).to.equal(true);
-    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(apiResult);
+    expect(utils.calculateNumber('SUM', 100, 20)).to.equal(apiResult);
     
     functionSpy.restore();
   });
