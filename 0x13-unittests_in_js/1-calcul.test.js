708d2bf3ffaaefd63a3d08403c7d5b0023c4a4b0
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 14:36:45 +0200

    Fix task 1

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -11,11 +11,12 @@ describe('calculateNumber with type === SUM', () => {
     assert.strictEqual(calculateNumber('SUM', 0.4, 8), 8);
     assert.strictEqual(calculateNumber('SUM', 10, 2.7), 13);
     assert.strictEqual(calculateNumber('SUM', 9.1, 9.9), 19);
+    assert.strictEqual(calculateNumber('SUM', 0.1, 0), 0);
   });
   it('checks output with negative numbers', () => {
     assert.strictEqual(calculateNumber('SUM', -1, -2), -3);
     assert.strictEqual(calculateNumber('SUM', -6, -3), -9);
-    assert.strictEqual(calculateNumber('SUM', -1.1, -2.3), -3);
+    assert.strictEqual(calculateNumber('SUM', 1.1, -2.3), -1);
     assert.strictEqual(calculateNumber('SUM', -5.8, -2), -8);
     assert.strictEqual(calculateNumber('SUM', -1, -8.4), -9);
     assert.strictEqual(calculateNumber('SUM', -1, 2.6), 2);
@@ -35,6 +36,7 @@ describe('calculateNumber with type === SUBSTRACT', () => {
     assert.strictEqual(calculateNumber('SUBSTRACT', 2.6, 2), 1);
     assert.strictEqual(calculateNumber('SUBSTRACT', 20.1, 8.8), 11);
     assert.strictEqual(calculateNumber('SUBSTRACT', 1, 9.1), -8);
+    assert.strictEqual(calculateNumber('SUBSTRACT', 0.0, 5), -5);
   });
   it('checks ouput with negative numbers', () => {
     assert.strictEqual(calculateNumber('SUBSTRACT', -1, 0), -1);
@@ -42,6 +44,8 @@ describe('calculateNumber with type === SUBSTRACT', () => {
     assert.strictEqual(calculateNumber('SUBSTRACT', -1.1, -7.9), 7);
     assert.strictEqual(calculateNumber('SUBSTRACT', -2.5, 0), -2);
     assert.strictEqual(calculateNumber('SUBSTRACT', -12, 14.3), -26);
+    assert.strictEqual(calculateNumber('SUBSTRACT', -1, 1), -2);
+    assert.strictEqual(calculateNumber('SUBSTRACT', -1.5, 0), -1);
   });
   it('checks with wrong argument', () => {
     assert.strictEqual(calculateNumber('SUBSTRACT', NaN, 2.2), NaN);
@@ -56,11 +60,13 @@ describe('calculateNumber with type == DIVIDE', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 10, 4), 2.5);
     assert.strictEqual(calculateNumber('DIVIDE', 4.5, 2.9), 1.6666666666666667);
     assert.strictEqual(calculateNumber('DIVIDE', 25, 3.2), 8.333333333333334);
+    assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
   });
   it('checks ouput with negative numbers', () => {
     assert.strictEqual(calculateNumber('DIVIDE', -6, -2), 3);
     assert.strictEqual(calculateNumber('DIVIDE', -10, 2), -5);
     assert.strictEqual(calculateNumber('DIVIDE', -7.7, -3.2), 2.6666666666666665);
+    assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
   });
   it('checks ouput with 0', () => {
     assert.strictEqual(calculateNumber('DIVIDE', 10, 0), 'Error');
