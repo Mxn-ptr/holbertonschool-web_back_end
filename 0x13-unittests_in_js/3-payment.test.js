1a163a78c296675c73cfbb03c7a6e86c180cdfcf
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 15:08:40 +0200

    Fix task 3

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -11,7 +11,7 @@ describe('sendPaymentRequestToApi', () => {
     
     const apiResult = sendPaymentRequestToApi(100, 20);
     
-    expect(functionSpy.calledOnceWith('SUM', 100, 20)).to.equal(true);
+    expect(functionSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
     expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
     expect(utils.calculateNumber('SUM', 100, 20)).to.equal(apiResult);
     
