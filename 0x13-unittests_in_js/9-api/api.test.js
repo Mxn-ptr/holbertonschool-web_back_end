8b30c0b2a1bc33c59d575a26a31b808d79548558
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 18:18:50 +0200

    Add task 9

---
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Index page', () => {
+  it('test the index page', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.statusMessage).to.equal('OK');
+        expect(response.request.method).to.equal('GET');
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      }
+    });
+  });
+  if('test with a correct endpoint', (done) => {
+    request('http://localhost:7865/cart/10', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(response.statusMessage).to.equal('OK');
+        expect(response.request.method).to.equal('GET');
+        expect(body).to.equal('Payment methods for cart 10');
+        done();
+      }
+    });
+  });
+  if('test with a wrong endpoint', (done) => {
+    request('http://localhost:7865/cart/wrong', (error, response, body) => {
+      if (response) {
+        expect(response.statusCode).to.equal(400);
+        done();
+      }
+    });
+  });
+});
