02252cf99dfda6f79aa4433407a367fc5a3e271c
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 15:11:59 +0200

    FIx task 3

--- 0x13-unittests_in_js/utils.js
+++ 0x13-unittests_in_js/utils.js
@@ -1,16 +1,13 @@
 const Utils = {
   calculateNumber(type, a, b) {
     if (!type) throw new TypeError('missing type argument');
-    switch (type) {
-      case 'SUM':
-        return Math.round(a) + Math.round(b);
-      case 'SUBSTRACT':
-        return Math.round(a) - Math.round(b);
-      case 'DIVIDE':
-        if (Math.round(b) === 0) return 'Error';
-        return Math.round(a) / Math.round(b);
-      default:
-        throw new TypeError;
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    } if (type === 'SUBSTRACT') {
+      return Math.round(a) - Math.round(b);
+    } if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) return 'Error';
+      return Math.round(a) / Math.round(b);
     }
   }
 };
