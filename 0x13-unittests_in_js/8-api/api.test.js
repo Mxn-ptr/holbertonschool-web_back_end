424128e26a8e198c80df45d9837b078473a47fbe
Mxn-ptr <maxence0@hotmail.fr>
2023-04-05 18:10:09 +0200

    Try to fix task 8

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,13 +2,14 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Index page', () => {
-  it('test the index page', () => {
+  it('test the index page', (done) => {
     request('http://localhost:7865', (error, response, body) => {
       if (response) {
         expect(response.statusCode).to.equal(200);
         expect(response.statusMessage).to.equal('OK');
         expect(response.request.method).to.equal('GET');
         expect(body).to.equal('Welcome to the payment system');
+        done();
       }
     });
   });
